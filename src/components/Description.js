50c1644dcae5cc59b30ef0a22b108e6c06a90cc6
Ajit Yadav <yadavajit8916@gmail.com>
2022-05-23 07:30:13 +0530

    code optimizations

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,34 +3,24 @@ import { getDescription } from "../services/api"
 import { useEffect, useState } from 'react';
 
 export const Description = ({sign,timeframe}) => {
-    const [description, setDescription] = useState([]);
-    const [luckyNumber , setLuckyNumber] = useState([]);
-    const [color , setColor] = useState([]);
-    const [compatibility , setCompatibility] = useState([]);
-    const [luckyTime , setLuckyTime]=useState([]);
-    const [mood, setMood] = useState([]);
-    const [dateRange , setDateRange] =useState([]);
-    useEffect( () => {
+   const [global,setGlobal] = useState({});
+       useEffect( () => {
+      
     getDescription(sign,timeframe).then((data) =>{
-      setDescription(data.description);
-      setLuckyNumber(data.lucky_number);
-      setColor(data.color);
-      setCompatibility(data.compatibility);
-      setLuckyTime(data.lucky_time);
-      setMood(data.mood);
-      setDateRange(data.date_range);
+      setGlobal(data)
     })
+
     });
   return (
     <div>
       <h1>{timeframe}, your Horoscope for {sign} is ... </h1>
-        <p>Description: {description}</p>
-    <p>luckyNumber : {luckyNumber} </p>
-    <p> Color :{color} </p>
-    <p>Compatibility : {compatibility}</p>
-    <p>Lucky Time : {luckyTime} </p>
-    <p>Mood : {mood} </p>
-    <p> Date Range : {dateRange} </p>
+        <p>Description: {global.description}</p>
+    <p>luckyNumber : {global.lucky_number} </p>
+    <p> Color :{global.color} </p>
+    <p>Compatibility : {global.compatibility}</p>
+    <p>Lucky Time : {global.lucky_time} </p>
+    <p>Mood : {global.mood} </p>
+    <p> Date Range : {global.date_range} </p>
     </div>
   )
 }
